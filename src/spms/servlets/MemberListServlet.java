09cb516e346ff82ed156687880b299cc7792a3cc
bbigbros <steveim1201@gmail.com>
2017-06-13 10:01:24 +0900

    MemberDao 생성 및 적용

--- src/spms/servlets/MemberListServlet.java
+++ src/spms/servlets/MemberListServlet.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import spms.dao.MemberDao;
 import spms.vo.Member;
 
 
@@ -23,44 +24,27 @@ public class MemberListServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
 
 		try {
+			// Connection이란 무엇인지? getServletContext의미란 무엇인지 다시 보기~
 			ServletContext sc = this.getServletContext();
-			conn = (Connection)sc.getAttribute("conn");
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select MNO, MNAME, EMAIL, CRE_DATE" + " from MEMBERS order by MNO ASC");
+			Connection conn = (Connection)sc.getAttribute("conn");
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(conn);
 			
-			response.setContentType("text/html; charset=UTF-8");
-			ArrayList<Member> members = new ArrayList<Member>();
-			// 데이터베이스에서 회원 정보를 가져와 Member에 담는다.
-			// 이후 members에 가져온 정보를 하나씩 넣는다.
-			while (rs.next()) {
-				members.add(
-						new Member().setNo(rs.getInt("MNO"))
-									.setName(rs.getString("MNAME"))
-									.setEmail(rs.getString("EMAIL"))
-									.setCreatedDate(rs.getDate("CRE_DATE"))
-						);
-			}
 			// requset에 회원정보를 보관한다.
-			request.setAttribute("members", members);
+			request.setAttribute("members",	memberDao.selectList());
+			response.setContentType("text/html; charset=UTF-8");
 			
 			// Dispatcher를 사용해 jsp로 출력을 위임한다.(include)
 			RequestDispatcher rd = request.getRequestDispatcher("/member/MemberList.jsp");
 			rd.include(request, response);
 			
 		} catch(Exception e) {
-//			throw new ServletException(e);
+			e.printStackTrace();
 			request.setAttribute("error", e);
-			// forward 방식
 			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
 			rd.forward(request, response);
-		} finally {
-			try { if (rs != null) rs.close(); } catch (Exception e){}
-			try { if (stmt != null) stmt.close(); } catch (Exception e){}
 		}
 	}
 
