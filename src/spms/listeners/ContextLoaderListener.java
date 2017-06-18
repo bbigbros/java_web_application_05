eda2b7ddf9076c1b9de5a142b55f3bceead4cfed
bbigbros <steveim1201@gmail.com>
2017-06-18 16:23:38 +0900

    톰캣서버에 DataSource 설정 및 적용

--- src/spms/listeners/ContextLoaderListener.java
+++ src/spms/listeners/ContextLoaderListener.java
@@ -2,31 +2,28 @@ package spms.listeners;
 
 import java.sql.SQLException;
 
+import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-
-import org.apache.commons.dbcp.BasicDataSource;
+import javax.sql.DataSource;
 
 import spms.dao.MemberDao;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
-	BasicDataSource ds;
 	
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		try {
 			System.out.println("ServletContextListener 시작");
 			ServletContext sc = event.getServletContext();
-
-			ds = new BasicDataSource();
-			ds.setDriverClassName(sc.getInitParameter("driver"));
-			ds.setUrl(sc.getInitParameter("url"));
-			ds.setUsername(sc.getInitParameter("username"));
-			ds.setPassword(sc.getInitParameter("password"));
-		
+			// 톰캣 서버 자원을 찾기위한 InitialContext 객체
+			// lookup()을 통해 JNDI이름으로 등록된 서버 자원을 찾을 수 있음
+			InitialContext initialContext = new InitialContext();
+			DataSource ds = (DataSource)initialContext.lookup("java:comp/env/jdbc/java_db");
+	
 			MemberDao memberDao = new MemberDao();
 			memberDao.setDataSource(ds);
 			
@@ -37,8 +34,5 @@ public class ContextLoaderListener implements ServletContextListener {
 	}
 	
 	@Override
-	public void contextDestroyed(ServletContextEvent event) {
-//		connPool.closeAll();
-		try { if (ds != null) ds.close(); } catch (SQLException e) {}
-	}
+	public void contextDestroyed(ServletContextEvent event) { }
 }
